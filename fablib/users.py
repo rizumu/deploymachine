1eb190e253a95b6ac637b3edbe1b71cabc0aec1f
Thomas Schreiber <tom@insatsu.us>
2011-04-16 15:54:28 +0200

    user add fixes

--- fablib/users.py
+++ fablib/users.py
@@ -9,24 +9,19 @@ from deploymachine.conf import settings
 
 def useradd(username):
     """
-    Adds a ``deploy`` user to the nodes.
+    Adds the ``deploy`` user to the nodes.
 
-    At one time DeployMachine handled creation of multiple user accounts, but
-    now it only creates a deploy user. Adding addtitional users is better
-    off handled in a kokki cookbook.
+    ...note At one time DeployMachine handled creation of multiple user
+    accounts, but now it only creates a single deploy user. Adding
+    addtitional users is best taken care of in a kokki cookbook.
     """
-    if username == settings.DEPLOY_USERNAME:
-        if settings.DOTFILE_REPOSITORY:
-            run("useradd --password {0} --groups wheel,sshers deploy".format(settings.DEPLOY_PASSWORD))
-            run("git clone {0} {1}".format(settings.DOTFILE_REPOSITORY, settings.DEPLOY_HOME))
-            append(join(settings.DEPLOY_HOME, ".git/info/exclude"), "\*") # exclude everything from git
-        else:
-            run("useradd --password {0} --create-home --groups wheel,sshers deploy".format(settings.PASSWORD))
-        with cd(settings.DEPLOY_HOME):
-            run("mkdir .virtualenvs")
-            run("mkdir --mode 700 .ssh/")
-        run("chsh --shell /bin/bash deploy")
-        ssh()
-        gitconfig()
-    else:
-        raise NotImplementedError()
+    if settings.DOTFILE_REPOSITORY:
+        run("useradd --password {0} --groups wheel,sshers deploy".format(settings.DEPLOY_PASSWORD_ENCRYPTED))
+        run("git clone {0} {1}".format(settings.DOTFILE_REPOSITORY, settings.DEPLOY_HOME))
+        append(join(settings.DEPLOY_HOME, ".git/info/exclude"), "\*") # exclude everything from git
+    with cd(settings.DEPLOY_HOME):
+        run("mkdir .virtualenvs")
+        run("mkdir --mode 700 .ssh/")
+    run("chsh --shell /bin/bash deploy")
+    ssh()
+    gitconfig()
