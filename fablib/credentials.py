c4d8c6682abbfa421b1c04705d0f48973b72a9de
Thomas Schreiber <tom@insatsu.us>
2011-05-06 03:51:05 +0200

    fix authorized_keys bug

--- fablib/credentials.py
+++ fablib/credentials.py
@@ -8,7 +8,7 @@ from deploymachine.conf import settings
 
 def ssh():
     """
-    Add or modify all of the machines SSH keys.
+    Add initial or update the machines SSH keys.
 
     For more info, see the installation documentation regarding SSH.
     """
@@ -18,17 +18,18 @@ def ssh():
         join(settings.DEPLOY_HOME, ".ssh/rackspacecloud_rsa.pub"), mode=0644)
     put(join(settings.DEPLOYMACHINE_LOCAL_ROOT, "ssh/config"),
         join(settings.DEPLOY_HOME, ".ssh/config"), mode=0600)
+    # HACK to put an empty authorized_keys which we'll personal pub keys are appended to
     put(join(settings.DEPLOYMACHINE_LOCAL_ROOT, "ssh/authorized_keys"),
         join(settings.DEPLOY_HOME, ".ssh/authorized_keys"), mode=0600)
     for admin in settings.ADMIN_SSH_KEYS:
+        sshkey = join(settings.DEPLOYMACHINE_LOCAL_ROOT, "ssh/", admin)
         append(join(settings.DEPLOY_HOME, ".ssh/authorized_keys"), "# {0}".format(admin))
-        append(join(settings.DEPLOY_HOME, ".ssh/authorized_keys"),
-               open(join(settings.DEPLOYMACHINE_LOCAL_ROOT, "ssh/", admin), "rb").read())
+        append(join(settings.DEPLOY_HOME, ".ssh/authorized_keys"), open(sshkey, "rb").read())
 
 
 def gitconfig():
     """
-    Add or modify all of the machines github config.
+    Add or update the machines git config with your private gihub settings.
     """
     run("> /home/deploy/.gitconfig")
     append("/home/deploy/.gitconfig", "\
