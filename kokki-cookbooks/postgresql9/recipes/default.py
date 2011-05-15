07135334fc6ea26759e2119888f4d07e2a661a59
Thomas Schreiber <tom@insatsu.us>
2011-05-16 01:45:36 +0200

    postgresql9 fixes

--- kokki-cookbooks/postgresql9/recipes/default.py
+++ kokki-cookbooks/postgresql9/recipes/default.py
@@ -1,15 +1,14 @@
 
 import os
-from kokki import Execute
+from kokki import Execute, Package
 
 apt_list_path = '/etc/apt/sources.list.d/pitti-postgresql-lucid.list'
 
-Execute("apt-update-postgresql9",
-    command = "apt-get update",
-    action = "nothing")
+Execute("apt-update-postgresql9", command = "apt-get update", action = "nothing")
 
 apt = None
 if env.system.platform == "ubuntu":
-    Execute("apt-add-repository ppa:pitti/postgresql",
+    Package("python-software-properties")
+    Execute("add-apt-repository ppa:pitti/postgresql",
         not_if = lambda:os.path.exists(apt_list_path),
         notifies = [("run", env.resources["Execute"]["apt-update-postgresql9"], True)])
