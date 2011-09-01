649947f208735fecb10f32251302bce82a4f39be
Thomas Schreiber <tom@insatsu.us>
2011-09-01 22:41:13 +0200

    use apt-get instead of aptitude and simplify provision step

--- contrib/provision.py
+++ contrib/provision.py
@@ -46,8 +46,14 @@ def provision():
     local("ssh {0}@{1} -o StrictHostKeyChecking=no &".format(env.user, env.host))
     run("sed -i 's/universe/universe multiverse/g' /etc/apt/sources.list")
     run("/usr/sbin/locale-gen en_US.UTF-8 && /usr/sbin/update-locale LANG=en_US.UTF-8")
-    run("aptitude update && aptitude -y safe-upgrade")
-    run("aptitude install -y {0}".format(" ".join(settings.BASE_PACKAGES)))
+    run("apt-get update && apt-get -y dist-upgrade")
+    run("apt-get install -y {0}".format(" ".join(settings.BASE_PACKAGES)))
+    run("pip install meld3")  # https://bugs.launchpad.net/ubuntu/+source/supervisor/+bug/777862
+    # vcprompt for system
+    with cd("/tmp/"):
+        run("git clone git://github.com/djl/vcprompt.git")
+    with cd("/tmp/vcprompt/"):
+        run("python setup.py install && rm -rf /tmp/vcprompt/")
     # deploy user setup
     run("groupadd wheel && groupadd sshers")
     run("cp /etc/sudoers /etc/sudoers.bak")
@@ -55,26 +61,17 @@ def provision():
     useradd("deploy")
     upload_template("templates/sshd_config.j2", "/etc/ssh/sshd_config",
                     context={"SSH_PORT": settings.SSH_PORT}, use_jinja=True)
-    # pip/virtualenv for system python
-    with cd("/tmp/"):
-        run("curl --remote-name http://python-distribute.org/distribute_setup.py")
-        run("python distribute_setup.py && rm distribute*")
-        run("git clone git://github.com/djl/vcprompt.git")
-    with cd("/tmp/vcprompt/"):
-        run("python setup.py install && rm -rf /tmp/vcprompt/")
-    run("easy_install pip && pip install virtualenv")
     # software configuration management
     if "kokki" in settings.CONFIGURATORS:
-        run("aptitude install -y python-jinja2")
         run("pip install git+git://github.com/samuel/kokki#egg=kokki \
                          git+git://github.com/jacobian/openstack.compute#egg=openstack_compute")
     if "chef" or "puppet" in settings.CONFIGURATORS:
-        run("aptitude install -y ruby-dev rubygems rdoc")
+        run("apt-get install -y ruby-dev rubygems rdoc")
         if "chef" in settings.CONFIGURATORS:
             run("gem install chef")
         if "puppet" in settings.CONFIGURATORS:
             if is_puppetmaster(public_ip=env.host):
-                run("aptitude install -y puppetmaster")
+                run("apt-get install -y puppetmaster")
             run("gem install puppet")
     run("chown -R {0}:{0} /home/{0}/".format("deploy"))
     # firewall + prevent root login
