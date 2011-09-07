2c92a0c7278e3377222ccd022d373f404dc754ce
Thomas Schreiber <tom@insatsu.us>
2011-09-07 15:04:17 +0200

    enhance git_pull to work locally

--- contrib/dvcs/git.py
+++ contrib/dvcs/git.py
@@ -1,27 +1,39 @@
-from fabric.api import env, cd, sudo
+from fabric.api import env, cd, lcd, local
 
 from deploymachine.conf import settings
 from deploymachine.contrib.supervisor import supervisor
-from deploymachine.contrib.fab import venv
+from deploymachine.contrib.fab import venv, venv_local
 
 
-def git_pull(site=None):
+def git_pull(connection, site=None):
     """
-    Update site(s) repository.
-        fab appnode git-pull:sitename
+    Checkout the site(s) repository.
+        fab git_pull:dev
+        fab git_pull:dev,sitename
+        fab appnode git_pull:prod
+        fab appnode git_pull:prod,sitename
     """
     if site is None:
         sites = [site["name"] for site in settings.SITES]
     else:
         sites = [site]
     for site in sites:
-        venv("git pull", site)
-        supervisor("gunicorn-{0}".format(site))
+        if connection == "dev":
+            venv_local("git pull", site)
+        elif connection == "prod":
+            venv("git pull", site)
+            supervisor("gunicorn-{0}".format(site))
+        else:
+            print("Bad connection type. Use ``dev`` or ``prod``.")
 
 
 def git_pull_deploymachine():
-    with cd(settings.DEPLOYMACHINE_ROOT):
-        sudo("git pull", user=settings.DEPLOY_USERNAME)
+    """
+    Checkout the local deploymachine repository.
+        fab appnode git_pull_deploymachine
+    """
+    with lcd(settings.DEPLOYMACHINE_LOCAL_ROOT):
+        local("git pull")
 
 
 def git_log(site=None):
