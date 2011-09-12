96683f01b41a497b132092f7fb1018f2055ac781
Thomas Schreiber <tom@insatsu.us>
2011-09-13 00:54:48 +0200

    fix up ppa handling in postgres recipe

--- kokki-cookbooks/postgresql9/recipes/default.py
+++ kokki-cookbooks/postgresql9/recipes/default.py
@@ -2,16 +2,18 @@
 import os
 from kokki import Execute, Package
 
-apt_list_path = '/etc/apt/sources.list.d/pitti-postgresql-lucid.list'
 
-Execute("apt-update-postgresql9", command = "apt-get update", action = "nothing")
+apt_list_path_pg = "/etc/apt/sources.list.d/pitti-postgresql-natty.list"
+apt_list_path_gis = "/etc/apt/sources.list.d/ubuntugis-ubuntugis-unstable-natty.list"
+
+Execute("apt-update-postgresql9", command="apt-get update", action="nothing")
 
 apt = None
 if env.system.platform == "ubuntu":
     Package("python-software-properties")
     Execute("add-apt-repository ppa:pitti/postgresql",
-        not_if = lambda:os.path.exists(apt_list_path),
+        not_if = lambda:os.path.exists(apt_list_path_pg),
         notifies = [("run", env.resources["Execute"]["apt-update-postgresql9"], True)])
     Execute("add-apt-repository ppa:ubuntugis/ubuntugis-unstable",
-        not_if = lambda:os.path.exists(apt_list_path),
+        not_if = lambda:os.path.exists(apt_list_path_gis),
         notifies = [("run", env.resources["Execute"]["apt-update-postgresql9"], True)])
