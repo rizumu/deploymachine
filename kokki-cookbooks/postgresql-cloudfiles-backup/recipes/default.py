5a0ced78bdbbba4e99891d2a2381168938550444
Thomas Schreiber <tom@insatsu.us>
2011-09-19 19:10:49 +0200

    use duplicity to backup dbdumps

--- kokki-cookbooks/postgresql-cloudfiles-backup/recipes/default.py
+++ kokki-cookbooks/postgresql-cloudfiles-backup/recipes/default.py
@@ -1,25 +1,32 @@
 import os
 from kokki import Directory, Package, File, Template, Service
 
-env.include_recipe("postgresql-cloudfiles-backup")
 
 Package("python-rackspace-cloudfiles")
+Package("duplicity")
 
-Directory("{0}db_backups/".format(env.config.cloudfiles.deploy_home),
+Directory("{0}/dbdumps/".format(env.config.cloudfiles.deploy_home),
     owner = "postgres",
     group = "deploy",
     mode = 0750)
 
-File("/etc/cron.d/pgdump",
+File("/etc/cron.d/dbdumps",
     owner = "postgres",
     group = "postgres",
     mode = 0644,
     content = Template("postgresql-cloudfiles-backup/cronfile.j2"),
 )
 
-File("{0}cloudfiles_uploader.py".format(env.config.cloudfiles.deploy_home),
+File("{0}/duplicity_uploader.sh".format(env.config.cloudfiles.deploy_home),
     owner = "postgres",
     group = "postgres",
     mode = 0740,
-    content = Template("postgresql-cloudfiles-backup/cloudfiles_uploader.py.j2"),
+    content = Template("postgresql-cloudfiles-backup/duplicity_uploader.sh.j2"),
+)
+
+File("{0}/duplicity_downloader.sh".format(env.config.cloudfiles.deploy_home),
+    owner = "postgres",
+    group = "postgres",
+    mode = 0740,
+    content = Template("postgresql-cloudfiles-backup/duplicity_downloader.sh.j2"),
 )
