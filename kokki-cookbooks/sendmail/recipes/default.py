d895650aaba7bea13ee8d9afdcc186a34ab9869e
Thomas Schreiber <tom@insatsu.us>
2011-09-20 11:29:26 +0200

    reload sendmail and config without prompt

--- kokki-cookbooks/sendmail/recipes/default.py
+++ kokki-cookbooks/sendmail/recipes/default.py
@@ -4,42 +4,39 @@ from kokki import Execute, File, Package, Service, Template
 env.include_recipe("sendmail")
 
 Service("sendmail",
-    supports_restart = True,
-    supports_reload = True,
-    supports_status = True,
-    action = "nothing")
+    supports_restart=True,
+    supports_reload=True,
+    supports_status=True,
+    action="nothing")
 
 Package("sasl2-bin")
 Package("sendmail")
 
-Execute("sendmailconfig", command="sendmailconfig", action="nothing")
+Execute("sendmailconfig", command="sendmailconfig --no-reload", action="nothing")
 Execute("newaliases", command="newaliases", action="nothing")
 
 File("/etc/mail/sendmail.mc",
-    owner = "root",
-    group = "smmsp",
-    mode = 0644,
-    content = Template("sendmail/sendmail.mc.j2"),
-    notifies = [("restart", env.resources["Service"]["sendmail"]),
-                ("run", env.resources["Execute"]["sendmailconfig"], True)],
-)
+    owner="root",
+    group="smmsp",
+    mode=0644,
+    content=Template("sendmail/sendmail.mc.j2"),
+    notifies=[("run", env.resources["Execute"]["sendmailconfig"], True),
+              ("restart", env.resources["Service"]["sendmail"])])
 
 File("/etc/mail/submit.mc",
-    owner = "root",
-    group = "smmsp",
-    mode = 0644,
-    content = Template("sendmail/submit.mc.j2"),
-    notifies = [("restart", env.resources["Service"]["sendmail"]),
-                ("run", env.resources["Execute"]["sendmailconfig"], True)],
-)
+    owner="root",
+    group="smmsp",
+    mode=0644,
+    content=Template("sendmail/submit.mc.j2"),
+    notifies=[("run", env.resources["Execute"]["sendmailconfig"], True),
+              ("restart", env.resources["Service"]["sendmail"])])
 
 File("/etc/aliases",
-    owner = "root",
-    group = "root",
-    mode = 0644,
-    content = Template("sendmail/aliases.j2"),
-    notifies = [("run", env.resources["Execute"]["newaliases"], True)],
-)
+    owner="root",
+    group="root",
+    mode=0644,
+    content=Template("sendmail/aliases.j2"),
+    notifies=[("run", env.resources["Execute"]["newaliases"], True)])
 
 # Unfortunately, there is no automagic way to migrate to /etc/sasldb2 :(
 # You'll want to make sure /etc/default/saslauthd is setup to start,
