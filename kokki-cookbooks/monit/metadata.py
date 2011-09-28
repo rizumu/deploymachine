866ad6cb8b90639c0eb2a06406143eb2cab29a5b
Thomas Schreiber <tom@insatsu.us>
2011-09-28 09:28:50 +0200

    configure monit

--- kokki-cookbooks/monit/metadata.py
+++ kokki-cookbooks/monit/metadata.py
@@ -2,17 +2,22 @@
 __description__ = "Process monitoring"
 __config__ = {
     "monit.alert_emails": dict(
-        display_name = "Alert emails",
-        description = "Emails that should receive alerts about service changes.",
-        default = [],
+        display_name="Alert emails",
+        description="Emails that should receive alerts about service changes.",
+        default=[],
     ),
     "monit.password": dict(
-        dispaly_name = "Password",
-        description = "Password for accessing web interface",
-        default = "m0n1t1tup",
+        dispaly_name="Password",
+        description="Password for accessing web interface",
+        default="m0n1t1tup",
     ),
-    'monit.config_path': dict(
-        description = "Path to config files",
-        default = "/etc/monit",
+    "monit.config_path": dict(
+        description="Path to config files",
+        default="/etc/monit",
+    ),
+    "monit.daemon_interval": dict(
+        description="Start monit in background (run as daemon) and check \
+                     the services at 2-minute intervals",
+        default="120",
     ),
 }
--- kokki-cookbooks/monit/recipes/default.py
+++ kokki-cookbooks/monit/recipes/default.py
@@ -1,29 +1,36 @@
-
 from kokki import Package, File, Template, Directory, Service
+
 
 Package("monit")
 
+
+Service("monit",
+    supports_status=False,
+    supports_restart=True,
+    action="nothing")
+
+
 File("%s/monitrc" % env.config.monit.config_path,
-    owner = "root",
-    group = "root",
-    mode = 0700,
-    content = Template("monit/monitrc.j2"))
+    owner="root",
+    group="root",
+    mode=0700,
+    content=Template("monit/monitrc.j2"),
+    notifies=[("restart", env.resources["Service"]["monit"])])
+
 
 if env.system.platform == "ubuntu":
     File("/etc/default/monit",
-        content = Template("monit/default.j2"))
+        content=Template("monit/default.j2"),
+        notifies=[("restart", env.resources["Service"]["monit"])])
+
 
 Directory("%s/monit.d" % env.config.monit.config_path,
-    owner = "root",
-    group = "root",
-    mode = 0700)
+    owner="root",
+    group="root",
+    mode=0700)
+
 
 Directory("/var/monit",
-    owner = "root",
-    group = "root",
-    mode = 0700)
-
-Service("monit",
-    supports_restart = True,
-    supports_status = False,
-    subscribes = [('restart', env.resources['File']["%s/monitrc" % env.config.monit.config_path])])
+    owner="root",
+    group="root",
+    mode=0700)
