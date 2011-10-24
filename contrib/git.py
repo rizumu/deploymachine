537fdea5a43c44b548694b864c434c798f418bbc
Thomas Schreiber <tom@insatsu.us>
2011-10-24 08:57:43 +0200

    always rebase when pulling locally

--- contrib/git.py
+++ contrib/git.py
@@ -4,6 +4,11 @@ from deploymachine.conf import settings
 from deploymachine.contrib.fab import venv, venv_local
 
 
+def git_pull_rebase():
+    return "alias git_current_branch=\"git symbolic-ref HEAD 2> /dev/null | sed -e 's/refs\/heads\///'\" && \
+        git fetch origin && git rebase --preserve-merges origin/$(git_current_branch)"
+
+
 def git_pull(connection, site=None):
     """
     Checkout the site(s) repository.
@@ -18,7 +23,7 @@ def git_pull(connection, site=None):
         sites = [site]
     for site in sites:
         if connection == "dev":
-            venv_local("git pull", site)
+            venv_local(git_pull_rebase(), site)
         elif connection == "prod":
             venv("git pull", site)
         else:
