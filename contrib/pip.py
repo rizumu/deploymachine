cfa190cb03596b44df2cb79701169aa220c9593f
Thomas Schreiber <tom@insatsu.us>
2011-11-13 02:02:59 +0100

    allow to pip_install from github

--- contrib/pip.py
+++ contrib/pip.py
@@ -1,4 +1,5 @@
 from fabric.api import local
+from fabric.colors import red, yellow, green
 
 from deploymachine.conf import settings
 from deploymachine.contrib.fab import venv, venv_local
@@ -15,7 +16,7 @@ def pip_requirements(connection, site=None):
     else:
         sites = [site]
     for site in sites:
-        print("started pip install for {0}".format(site))
+        print(green("started pip install for {0}".format(site)))
         if connection == "dev":
             venv_local("pip install --upgrade pip", site)
             venv_local("pip install --quiet --requirement=requirements.txt", site)
@@ -23,8 +24,8 @@ def pip_requirements(connection, site=None):
             venv("pip install --upgrade pip", site)
             venv("pip install --quiet --requirement=requirements.txt", site)
         else:
-            print("Bad connection type. Use ``dev`` or ``prod``.")
-        print("finished pip install for {0}".format(site))
+            print(red("Bad connection type. Use ``dev`` or ``prod``."))
+        print(green("finished pip install for {0}".format(site)))
 
 
 def pip_install(connection, repo, package, path=None, version=None, site=None):
@@ -34,7 +35,10 @@ def pip_install(connection, repo, package, path=None, version=None, site=None):
         fab pip_install:dev,git,django-oauth-access,git://github.com/eldarion,site=rizumu
         fab appnode pip_install:prod,pypi,Django,version=1.2.3,site=rizumu
     """
-    if repo in ["git", "hg"]:
+    if repo in ["git", "hg", "github"]:
+        if repo == "github":
+            path = "git://github.com/{0}".format(path)
+            repo = "git"
         fmt_egg = "{0}+{1}/{2}".format(repo, path, package)
         if version:
             fmt_egg += "@{0}".format(version)
@@ -44,7 +48,8 @@ def pip_install(connection, repo, package, path=None, version=None, site=None):
         if version:
             fmt_egg += "=={0}".format(version)
     else:
-        print("Repo type does not exist, use git, hg, or pypi")
+        print(red("Repo type does not exist, use git, hg, or pypi"))
+        raise(NotImplementedError)
     if site is None:
         sites = [site["name"] for site in settings.SITES]
     else:
@@ -55,7 +60,7 @@ def pip_install(connection, repo, package, path=None, version=None, site=None):
         elif connection == "prod":
             venv("pip install --ignore-installed {0}".format(fmt_egg), site)
         else:
-            print("Bad connection type. Use ``dev`` or ``prod``.")
+            print(red("Bad connection type. Use ``dev`` or ``prod``."))
 
 
 def pip_uninstall(connection, package, site=None):
@@ -76,6 +81,6 @@ def pip_uninstall(connection, package, site=None):
             elif connection == "prod":
                 venv("pip uninstall --yes {0}".format(package), site)
             else:
-                print("Bad connection type. Use ``dev`` or ``prod``.")
+                print(red("Bad connection type. Use ``dev`` or ``prod``."))
         except:
-            print('Cannot uninstall requirement {0} on site {1}, not installed'.format(package, site))
+            print(red('Cannot uninstall requirement {0} on site {1}, not installed'.format(package, site)))
